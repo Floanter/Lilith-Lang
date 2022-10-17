f7ba36781ce64806b8baa0d7f389fa2d2c4b86dd
Floanter <99696724+Floanter@users.noreply.github.com>
2022-10-16 21:51:07 -0600

    full variable parsing

--- lilith.py
+++ lilith.py
@@ -3,5 +3,5 @@ from src.Parser import LilithParser
 
 parser = LilithParser()
 
-with open('examples/function.li', 'r') as f:
+with open('examples/variables.li', 'r') as f:
     print(parser.parsePretty(f.read()))
