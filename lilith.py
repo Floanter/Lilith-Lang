17ce5ebb02e260943ae2aea14b692db93ece109b
Floanter <brianfloant@gmail.com>
2022-10-19 13:53:54 -0600

    if statements

--- lilith.py
+++ lilith.py
@@ -3,5 +3,5 @@ from src.Parser import LilithParser
 
 parser = LilithParser()
 
-with open('examples/variables.li', 'r') as f:
+with open('examples/conditions.li', 'r') as f:
     print(parser.parsePretty(f.read()))
