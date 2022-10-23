e3c3595d8f24deb7299f77669edcd0e9ab3bf254
Floanter <99696724+Floanter@users.noreply.github.com>
2022-10-22 21:13:26 -0600

    classes!

--- lilith.py
+++ lilith.py
@@ -2,6 +2,6 @@ from src.Parser import LilithParser
 
 parser = LilithParser()
 
-with open('examples/namespace.spell', 'r') as f:
+with open('examples/class.spell', 'r') as f:
     tokens = parser.parse(f.read())
     print(tokens.pretty())
