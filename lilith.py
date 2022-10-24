9c370e083a486469f2d1545bfd026b629f79fe58
Floanter <brianfloant@gmail.com>
2022-10-24 14:09:43 -0600

    making io lib

--- lilith.py
+++ lilith.py
@@ -2,6 +2,6 @@ from src.Parser import LilithParser
 
 parser = LilithParser()
 
-with open('examples/templates.spell', 'r') as f:
+with open('examples/io.spell', 'r') as f:
     tokens = parser.parse(f.read())
     print(tokens.pretty())
