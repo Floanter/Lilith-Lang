ec408319f99ca3df1511a6000cea2d9c7c7740dc
Floanter <99696724+Floanter@users.noreply.github.com>
2022-11-04 09:39:38 -0600

    vars

--- src/CodeConverter.py
+++ src/CodeConverter.py
@@ -1,4 +1,3 @@
-from ast import arguments
 from src.Templates import Template
 from src.Parser import LilithParser
 
@@ -43,9 +42,38 @@ class CodeConverter():
                 #          FUNCTIONS       #
                 if blockType == 'main_function':
                     block += self._main_function(b)
+
                 if blockType == 'function_definition':
                     block += self._function_definition(b)
+
+                if blockType == 'function_declaration':
+                    block += self._function_declaration(b)
+
+                if blockType == 'call':
+                    block += self._call(b)
+
+                if blockType == 'call_params':
+                    block += self._call_params(b)
                 #          FUNCTIONS       #
+
+                #          VARIABLES       #
+                if blockType == 'var_declaration_assign':
+                    block += self._var_declaration_assign(b)
+
+                if blockType == 'var_declaration':
+                    block += self._var_declaration(b)
+
+                if blockType == 'var_reassign':
+                    block += self._var_reassign(b)
+
+                if blockType == 'var_reassign_value':
+                    block += self._var_reassign_value(b)
+                #          VARIABLES       #
+
+                #        CONDITIONALS      #
+                if blockType == 'if_condition':
+                    block += self._if_condition(b)
+                #        CONDITIONALS      #
 
         return block
 
@@ -143,13 +171,63 @@ class CodeConverter():
         for b in blocks.children:
             if hasattr(b, 'data'):
                 blockType = b.data
-                print(blockType)
+                #print(blockType)
+                if blockType == 'type':
+                    type += self._type(b)
+        
+                if blockType == 'identifier':
+                    identifier += self._identifier(b)
+                
                 if blockType == 'arguments':
                     arguments += self._arguments(b)
 
                 if blockType == 'block':
                     inside += self._block(b)
-        return ''
+
+        return self.templates.function(type, identifier, arguments, inside)
+
+    def _function_declaration(self, blocks):
+        type = ''
+        identifier = ''
+        arguments = ''
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                #print(blockType)
+                if blockType == 'type':
+                    type += self._type(b)
+        
+                if blockType == 'identifier':
+                    identifier += self._identifier(b)
+                
+                if blockType == 'arguments':
+                    arguments += self._arguments(b)
+
+        return self.templates.declare(type, identifier, arguments)
+
+    def _call(self, blocks, semicolon= True):
+        identifier = ''
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                #print(blockType)
+                if blockType == 'identifier':
+                    identifier += self._identifier(b)
+        return self.templates.call(identifier, semicolon)
+
+    def _call_params(self, blocks, semicolon=True):
+        identifier = ''
+        parameters = ''
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                #print(blockType)
+                if blockType == 'identifier':
+                    identifier += self._identifier(b)
+
+                if blockType == 'parameters':
+                    parameters += self._parameters(b)
+        return self.templates.callWithParams(identifier, parameters, semicolon)
 
     def _arguments(self, blocks):
         arguments = []
@@ -180,12 +258,203 @@ class CodeConverter():
                     type += self._embed_type(b)
        
         return self.templates.argument(type, identifier)
+
+    def _parameters(self, blocks):
+        parameters = []
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                #print(blockType)
+                if blockType == 'value':
+                    parameters.append(self._value(b))
+
+                if blockType == 'arithmetic':
+                    parameters.append(self._arithmetic(b))
+        return self.templates.parameters(parameters)
     ################################################################
     ################################################################
     #                         FUNCTION                             #
     ################################################################
     ################################################################
 
+
+    ################################################################
+    ################################################################
+    #                        CONDITIONAL                           #
+    ################################################################
+    ################################################################
+    def _if_condition(self, blocks):
+        conditions = []
+        inside = ''
+        logicals = []
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                print(blockType)
+                if blockType == 'condition':
+                    condition += self._condition(b)
+
+                if blockType == 'block':
+                    inside += self._block(b)
+
+                if blockType == 'or_logical':
+                    logicals.append('||')
+
+        return self.templates.if_condition(condition, inside)
+
+    def _condition(self, blocks):
+        values = []
+        condition = ''
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                #print(blockType)
+                if blockType == 'value':
+                    values.append(self._value(b))
+                
+                if blockType == 'greater_comparison':
+                    condition += '>'
+
+                if blockType == 'less_comparison':
+                    condition += '<'
+        return self.templates.condition(values, condition)
+    ################################################################
+    ################################################################
+    #                        CONDITIONAL                           #
+    ################################################################
+    ################################################################
+
+
+    ################################################################
+    ################################################################
+    #                         VARIABLE                             #
+    ################################################################
+    ################################################################
+    def _var_declaration_assign(self, blocks):
+        var = ''
+        value = ''
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                #print(blockType)
+                if blockType == 'normal_var_type':
+                    var += self._normal_var_type(b)
+
+                if blockType == 'const_var_type':
+                    var += self._const_var_type(b)
+
+                if blockType == 'static_var_type':
+                    var += self._static_var_type(b)
+                
+                if blockType == 'value':
+                    value += self._value(b)
+
+        return self.templates.var_declaration_assign(var, value)
+
+    def _var_declaration(self, blocks):
+        var = ''
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                #print(blockType)
+                if blockType == 'normal_var_type':
+                    var += self._normal_var_type(b)
+
+                if blockType == 'const_var_type':
+                    var += self._const_var_type(b)
+
+                if blockType == 'static_var_type':
+                    var += self._static_var_type(b)
+
+        return self.templates.var_declaration(var)
+
+    def _var_reassign(self, blocks):
+        identifier = ''
+        reassign_operator = ''
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                #print(blockType)
+            if blockType == 'identifier':
+                identifier += self._identifier(b)
+
+            if blockType == 'reassign_operator':
+                reassign_operator += self._reassign_operator(b)
+
+        return self.templates.var_reassign(identifier, reassign_operator)
+
+    def _var_reassign_value(self, blocks):
+        identifier = ''
+        reassign_operator = ''
+        value = ''
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                #print(blockType)
+            if blockType == 'identifier':
+                identifier += self._identifier(b)
+
+            if blockType == 'reassign_operator':
+                reassign_operator += self._reassign_operator(b)
+
+            if blockType == 'value':
+                value += self._value(b)
+
+            if blockType == 'arithmetic':
+                value += self._arithmetic(b)
+                
+        return self.templates.var_reassign_value(identifier, reassign_operator, value)
+
+    def _normal_var_type(self, blocks):
+        type = ''
+        identifier = ''
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                #print(blockType)
+                if blockType == 'identifier':
+                    identifier += self._identifier(b)
+                
+                if blockType == 'type':
+                    type += self._type(b)
+                
+        return f'{type} {identifier}'
+
+    def _const_var_type(self, blocks):
+        type = ''
+        identifier = ''
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                #print(blockType)
+                if blockType == 'identifier':
+                    identifier += self._identifier(b)
+                
+                if blockType == 'type':
+                    type += self._type(b)
+                
+        return f'const {type} {identifier}'
+
+    def _static_var_type(self, blocks):
+        type = ''
+        identifier = ''
+        for b in blocks.children:
+            if hasattr(b, 'data'):
+                blockType = b.data
+                #print(blockType)
+                if blockType == 'identifier':
+                    identifier += self._identifier(b)
+                
+                if blockType == 'type':
+                    type += self._type(b)
+                
+        return f'static {type} {identifier}'
+
+    ################################################################
+    ################################################################
+    #                         VARIABLE                             #
+    ################################################################
+    ################################################################
 
 
     ################################################################
@@ -198,6 +467,61 @@ class CodeConverter():
         for i in blocks.children:
             identifier += i
         return identifier
+
+    def _value(self, blocks):
+        value = ''
+        for t in blocks.children:
+            if hasattr(t, 'data'):
+                tokenType = t.data
+                #print(tokenType)
+                if tokenType == 'identifier':
+                    value += self._identifier(t)
+
+                if tokenType == 'call':
+                    value += self._call(t, False)
+
+                if tokenType == 'call_params':
+                    value += self._call_params(t, False)
+            else:
+                #print(t)
+                value += t
+        return value
+
+    def _arithmetic(self, blocks):
+        first_value = ''
+        arith_value = ''
+        for t in blocks.children:
+            if hasattr(t, 'data'):
+                tokenType = t.data
+                #print(tokenType)
+                if tokenType == 'first_value':
+                    first_value += self._value(t)
+
+                if tokenType == 'arith_value':
+                    arith_value += self._arith_value(t)
+
+        return first_value + arith_value
+
+    def _arith_value(self, blocks):
+        v = ''
+        for t in blocks.children:
+            if hasattr(t, 'data'):
+                tokenType = t.data
+                print(tokenType)
+                if tokenType == 'sign':
+                    v += self._sign(t)
+
+                if tokenType == 'call':
+                    v += self._call(t, False)
+            else:
+                v += t
+        return v
+
+    def _sign(self, blocks):
+        s = ''
+        for t in blocks.children:
+            s += t
+        return s
 
     def _type(self, blocks):
         type = ''
@@ -215,6 +539,12 @@ class CodeConverter():
             else:
                 type += t
         return f'{type}<{embed_type}>'
+
+    def _reassign_operator(self, blocks):
+        o = ''
+        for t in blocks.children:
+            o += t
+        return o
     ################################################################
     ################################################################
     #                           VALUES                             #
--- src/Templates.py
+++ src/Templates.py
@@ -27,6 +27,22 @@ class Template:
         function = f'void _MaInLiLith_({arguments})\n{{\n{inside}\n}}\n'
         return function
 
+    def function(self, type, identifier, arguments, inside):
+        return f'{type} {identifier}({arguments})\n{{\n{inside}\n}}\n'
+
+    def declare(self, type, identifier, arguments):
+        return f'{type} {identifier}({arguments});\n'
+
+    def call(self, identifier, semicolon = True):
+        if semicolon:
+            return f'{identifier}();\n'
+        return f'{identifier}()'
+
+    def callWithParams(self, identifier, parameters, semicolon=True):
+        if semicolon:
+            return f'{identifier}({parameters});\n'
+        return f'{identifier}({parameters})'
+
     def argument(self, type, identifier):
         return f'{type} {identifier}'
 
@@ -36,12 +52,63 @@ class Template:
             for i in range(1, len(arguments)):
                 a += f', {arguments[i]}'
         return a
+
+    def parameters(self, parameters):
+        p = f'{parameters[0]}'
+        if len(parameters) > 1:
+            for i in range(1, len(parameters)):
+                p += f', {parameters[i]}'
+        return p
     ################################################################
     ################################################################
     #                         FUNCTION                             #
     ################################################################
     ################################################################
 
+
+
+    ################################################################
+    ################################################################
+    #                        CONDITIONAL                           #
+    ################################################################
+    ################################################################
+    def if_condition(self, condition, inside):
+        return f'if({condition})\n{{\n{inside}\n}}\n'
+
+    def condition(self, values, condition):
+        if len(values) > 1:
+            return f'{values[0]} {condition} {values[1]}'
+        return f'{values[0]}'
+    ################################################################
+    ################################################################
+    #                        CONDITIONAL                           #
+    ################################################################
+    ################################################################
+
+
+
+
+    ################################################################
+    ################################################################
+    #                         VARIABLE                             #
+    ################################################################
+    ################################################################
+    def var_declaration_assign(self, var, value):
+        return f'{var} = {value};\n'
+
+    def var_declaration(self, var):
+        return f'{var};\n'
+
+    def var_reassign_value(self, identifier, reassign_operator, value):
+        return f'{identifier} {reassign_operator} {value};\n'
+
+    def var_reassign(self, identifier, reassign_operator):
+        return f'{identifier}{reassign_operator};\n'
+    ################################################################
+    ################################################################
+    #                         VARIABLE                             #
+    ################################################################
+    ################################################################
 
 
     ################################################################
